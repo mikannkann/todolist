a9164de340259599239d057a9e48d5ec1c4866ba
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-06-13 18:14:36 +0900

    feat: 商品リスト

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import ItemList from './components/ItemList.vue';
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
@@ -9,6 +10,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <ItemList />
     </div>
   </header>
 
@@ -16,32 +18,3 @@ import TheWelcome from './components/TheWelcome.vue'
     <TheWelcome />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,50 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+import { ref } from 'vue'
+
+interface Item {
+  name: string
+  price: number
+}
+
+const items = ref<Item[]>([
+  { name: 'たまご', price: 100 },
+  { name: 'りんご', price: 160 }
+])
+const newItemName = ref('')
+const newItemPrice = ref(0)
+
+const addItem = () => {
+  items.value.push({ name: newItemName.value, price: newItemPrice.value })
+}
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div>
+    <div>ItemList</div>
+    <ul>
+      <li v-for="item in items" :key="item.name" :class="{ over500: item.price >= 500 }">
+        <div >名前: {{ item.name }}</div>
+        <div>{{ item.price }} 円</div>
+      </li>
+    </ul>
+    <div>
+      <label>
+        名前
+        <input v-model="newItemName" type="text" />
+      </label>
+      <label>
+        価格
+        <input v-model="newItemPrice" type="number" />
+      </label>
+      <button @click="addItem">add</button>
+    </div>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+<style>
+.over500 {
+  color: red;
 }
 </style>
+
+<style></style>
